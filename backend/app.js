0a22e3145d66d6a0cc4e6fa57b1e9cc58d1dc60c
Gramoschi Rafael <gramoschi.rafael@gmail.com>
2024-11-07 07:02:48 +0100

    Change users view

--- backend/app.js
+++ backend/app.js
@@ -19,8 +19,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+/** ROUTES **/
+
+
+// INDEX
 app.use('/', indexRouter);
+
+
+// USERS
 app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const users = [
+  {
+    id: 123456,
+    fullname: "Tom",
+    email: "tom@email.loc",
+    password: "secret",
+  },
+  {
+    id: 123457,
+    fullname: "Harry",
+    email: "harry@email.loc",
+    password: "secret",
+  },
+  {
+    id: 123458,
+    fullname: "Tim",
+    email: "tim@email.loc",
+    password: "secret",
+  },
+];
 /** BASE PATH IS /users */
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json({ users: "Get users list"});
+  res.json({ result: users });
 });
 
 module.exports = router;
